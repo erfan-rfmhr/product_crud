2112f84c5dbf901a8c75365aad3b456b6465d55b
erfan <erfan.arefmehr@gmail.com>
2022-12-19 11:22:57 +0330

    make product class

---
+++ product.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+class Product:
+    __products = {} # stores object and id
+    __auto_increment_id = -1
+    
+    def __is_valid(value) -> bool:
+        """Return True if value is a floating point number """
+        try:
+            float(value)
+            return True
+        except:
+            return False
+    
+    def __init__(self, title:str, short_description:str='', description:str='',
+                 slug:str='', permalink:str='', price:float=0, regular_price:float=0,
+                 sale_price:float=0, manage_stoke:int=0, stoke_quantity:int=0,
+                 isAvailable=True, isVisible=True):
+        
+        Product.__auto_increment_id += 1
+        self.__id = Product.__auto_increment_id
+        self.title = title
+        self.short_description = short_description
+        self.description = description
+        self.slug = slug
+        self.permalink = permalink
+        self.isAvailable = isAvailable
+        self.price = price
+        self.regular_price = regular_price
+        self.sale_price = sale_price
+        self.manage_stoke = manage_stoke
+        self.stoke_quantity = stoke_quantity
+        self.isVisible = isVisible
+        self.date_created_gmt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.date_modified_gmt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+    def add_product(self):
+        Product.__products[self] = self.__id
+        
+    @property
+    def id(self):
+        return self.__id
+    
+    def delete_product(self):
+        """Delete a product"""
+        del Product.__products[self]
+        print('-'*5,f'\nProduct {self.title} (id {self.__id}) has been deleted','-'*5)
+        
+    @staticmethod
+    def get_all_products():
+        return tuple(Product.__products.keys())
+    
+    def get_info(self):
+        """Return all attributes as a dictionary"""
+        return self.__dict__
+        
+    def __repr__(self) -> str:
+        return self.title + ' ' + 'id:' + str(self.__id)
