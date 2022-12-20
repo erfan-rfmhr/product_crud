3940d68f0eae3a6ba53865148119447936f1b961
erfan <erfan.arefmehr@gmail.com>
2022-12-20 12:12:26 +0330

    type and instance

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from product import Product
+
+p = Product('test')
+
+print(type(Product))    # type of every class in python is "type"
+print(type(p))
+print(isinstance(p, Product))   # output is True because p is an instance of Product class
--- product.py
+++ product.py
@@ -3,20 +3,17 @@ from datetime import datetime
 class Product:
     __products = {} # stores object and id
     __auto_increment_id = -1
-    
-    def __is_valid(value) -> bool:
-        """Return True if value is a floating point number """
-        try:
-            float(value)
-            return True
-        except:
-            return False
-    
+        
     def __init__(self, title:str, short_description:str='', description:str='',
                  slug:str='', permalink:str='', price:float=0, regular_price:float=0,
                  sale_price:float=0, manage_stoke:int=0, stoke_quantity:int=0,
                  isAvailable=True, isVisible=True):
         
+        assert price>=0, 'price is not greater than or equal to 0'
+        assert regular_price>=0, 'regular price is not greater than or equal to 0'
+        assert sale_price>=0, 'sale price is not greater than or equal to 0'
+        assert manage_stoke>=0, 'manage_stoke is not greater than or equal to 0'
+        assert stoke_quantity>=0, 'stoke_quantity is not greater than or equal to 0'
         Product.__auto_increment_id += 1
         self.__id = Product.__auto_increment_id
         self.title = title
@@ -35,19 +32,24 @@ class Product:
         self.date_modified_gmt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
     def add_product(self):
+        """Add the current product to list of products"""
         Product.__products[self] = self.__id
         
     @property
-    def id(self):
+    def user_id(self):
         return self.__id
     
     def delete_product(self):
-        """Delete a product"""
-        del Product.__products[self]
-        print('-'*5,f'\nProduct {self.title} (id {self.__id}) has been deleted','-'*5)
-        
+        """Delete the current product object"""
+        try:
+            del Product.__products[self]
+            print('-'*5,f'\nProduct {self.title} (id {self.__id}) has been deleted','-'*5)
+        except:
+            print('-'*5,f'\nProduct {self.title} (id {self.__id}) has already been deleted','-'*5)
+            
     @staticmethod
     def get_all_products():
+        """return all products from list of products"""
         return tuple(Product.__products.keys())
     
     def get_info(self):
